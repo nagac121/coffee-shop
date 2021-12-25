3f97a5284574efb024b1dc4fafc6cac1f44c8ad2
naga <nagac121@gmail.com>
2021-12-25 11:05:48 +0530

    menu created

--- src/App.js
+++ src/App.js
@@ -1,13 +1,54 @@
 import "./App.css";
 import Menu from "./components/Menu";
 
+const initialMenu = [
+  {
+    itemName: "bread",
+    cost: "3",
+    offer: "pay",
+    tax: "1%",
+    discount: "0%",
+    combo: [
+      {
+        item: "coffee",
+        discount: "2%",
+      },
+      {
+        item: "juice",
+        discount: "3%",
+      },
+    ],
+  },
+  {
+    itemName: "coffee",
+    cost: "10",
+    offer: "pay",
+    tax: "2%",
+    discount: "0%",
+  },
+  {
+    itemName: "juice",
+    cost: "15",
+    offer: "pay",
+    tax: "2%",
+    discount: "0%",
+  },
+  {
+    itemName: "cream",
+    cost: "0",
+    offer: "free",
+    tax: "0%",
+    discount: "0%",
+  },
+];
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>BARISTA</div>
       </header>
-      <Menu />
+      <Menu menuList={initialMenu} className="menu" />
     </div>
   );
 }
--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,4 +1,29 @@
-const Menu = () => {
-  return <div>menu</div>
-}
+import Typography from "@mui/material/Typography";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemText from "@mui/material/ListItemText";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
+
+const Menu = ({ menuList }) => {
+  console.log("menu: ", menuList);
+  return (
+    <Box display="flex" flexDirection="column" alignItems="center" padding={1}>
+      <Typography sx={{ mt: 4, mb: 2 }} variant="h6" component="div">
+        menu
+      </Typography>
+      {/* <Stack spacing={2} alignItems="center"> */}
+      <List dense={true}>
+        {menuList.map((menuItem, index) => {
+          return (
+            <ListItem key={index}>
+              <ListItemText primary={menuItem.itemName} />
+            </ListItem>
+          );
+        })}
+      </List>
+      {/* </Stack> */}
+    </Box>
+  );
+};
 export default Menu;
