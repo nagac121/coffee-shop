d0818509d1ad7fb8037c5bf8006eb0c7f2be5f32
naga <nagac121@gmail.com>
2021-12-25 17:46:01 +0530

    cart items dispalyed

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,34 @@
-const Cart = () => {
-  return <div>
-    cart data
-  </div>
-}
+import IconButton from "@mui/material/IconButton";
+import AddSharpIcon from "@mui/icons-material/AddSharp";
+import RemoveSharpIcon from "@mui/icons-material/RemoveSharp";
+
+const Cart = ({ cartData }) => {
+  console.log("cart data: ", cartData);
+  return (
+    <div className="cart">
+      <header>CART ITEMS</header>
+      {cartData.map((cartItem, index) => {
+        return (
+          <div className="cart__item" key={index}>
+            <span>{cartItem.itemName}</span>
+            <span className="cart-qty-section">
+              <IconButton color="primary">
+                <RemoveSharpIcon className="cart__item--qty-icon" />
+              </IconButton>
+              <input
+                className="cart__item--qty"
+                disabled={true}
+                type="text"
+                value=""
+              />
+              <IconButton color="primary">
+                <AddSharpIcon className="cart__item--qty-icon" />
+              </IconButton>
+            </span>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 export default Cart;
